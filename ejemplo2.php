5a6f72ccd0f70b17eb7fcc0778d401cda2fb4107
Hector <202100365b@utea.edu.pe>
2024-01-11 09:50:17 -0500

    2commitas

--- ejemplo2.php
+++ ejemplo2.php
@@ -4,8 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP - HTML5</title>
+    <link rel="stylesheet" href="estilo.css">
 </head>
 <body>
-    
+    <header>
+        <h3>fundamentos de programacion web</h3>
+    <header>
+</section>
+<?php
+echo "<br>1. Ingresar codigo PHP en HTMLS5";
+echo "<br>2. Pagina estatica versus Pagina Dinamica";
+echo "<br>3. Manejo de variables en PHP";
+echo "<br>4. Danny entiende que Wendy no te ama";
+echo "<br>5. Esto es algo epicoxd";
+echo "<br>6. Ale despegate de Danny XD";
+?>
+<footer>
+    <h6>TODOS LOS DERECHOS RESERVADOS YKZ </h6>
 </body>
 </html>
